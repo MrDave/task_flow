14ada6fcba138f117ea0983526918c76fe62233f
David Schnoll <davidschnoll@gmail.com>
2025-11-25 12:36:24 +0200

    Filter tasks by owner

--- task_flow_api/tasks/views.py
+++ task_flow_api/tasks/views.py
@@ -1,22 +1,23 @@
-from django.shortcuts import render
-
 from django.contrib.auth.models import User
-
 from rest_framework import generics, permissions
 
 from tasks.models import Task
-from tasks.serializers import TaskSerializer, UserSerializer
 from tasks.permissions import IsOwnerOrReadOnly
+from tasks.serializers import TaskSerializer, UserSerializer
 
 
 class TaskList(generics.ListCreateAPIView):
-    queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        """This view should return a list of all the tasks for the currently authenticated user."""
+        user = self.request.user
+        return Task.objects.filter(owner=user)
 
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
