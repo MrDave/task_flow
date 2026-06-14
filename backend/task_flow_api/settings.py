b18de2cf8e102551ed5dbe69776f8ff034bcd5bf
David Schnoll <davidschnoll@gmail.com>
2026-06-14 21:21:32 +0300

    Configure static settings

--- backend/task_flow_api/settings.py
+++ backend/task_flow_api/settings.py
@@ -122,6 +122,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = Path(BASE_DIR).joinpath("collected_static")
+
+MEDIA_ROOT = Path(BASE_DIR).joinpath("media")
+
+MEDIA_URL = "media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
